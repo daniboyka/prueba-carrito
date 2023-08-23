790842615e6287bc8cc4db08d050615e1a94425b
daniboyka <danielcaz@gmail.com>
2023-08-23 00:00:04 -0300

    prueba de carrito

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Probando } from './componente/probando'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Probando/>
+  </React.StrictMode>,
+)
